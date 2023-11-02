926609e7b9956be2271160f8642e7c61580c967b
Daniella <elisiella13@gmail.com>
2023-11-03 00:32:09 +0100

    Ok

--- the_files/myCheckout.js
+++ the_files/myCheckout.js
@@ -1,20 +1,20 @@
-// // Get the selected values from local storage
-// const selectedValues = JSON.parse(localStorage.getItem('selectedValues'));
+// Get the selected values from local storage
+const selectedValues = JSON.parse(localStorage.getItem('selectedValues'));
 
-// // Display the selected values
-// const selectedValuesDiv = document.querySelector('#selectedValues');
+// Display the selected values
+const selectedValuesDiv = document.querySelector('#selectedValues');
 
-// if (selectedValues) {
-//     const selectedOptions = document.createElement('div');
+if (selectedValues) {
+    const selectedOptions = document.createElement('div');
 
-//     for (const key in selectedValues) {
-//         const value = selectedValues[key];
-//         const optionText = document.createElement('p');
-//         optionText.textContent = `${key}: ${value}`;
-//         selectedOptions.appendChild(optionText);
-//     }
+    for (const key in selectedValues) {
+        const value = selectedValues[key];
+        const optionText = document.createElement('p');
+        optionText.textContent = `${key}: ${value}`;
+        selectedOptions.appendChild(optionText);
+    }
 
-//     selectedValuesDiv.appendChild(selectedOptions);
-// } else {
-//     selectedValuesDiv.innerHTML = '<p>No options selected.</p>';
-// }
+    selectedValuesDiv.appendChild(selectedOptions);
+} else {
+    selectedValuesDiv.innerHTML = '<p>No options selected.</p>';
+}
